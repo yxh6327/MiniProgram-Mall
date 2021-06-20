299891bb2bb518af8001ca656fa7cae231039f2e
yxh6327 <1470364228@qq.com>
2021-06-20 17:36:33 +0800

    sixth commit

---
+++ pages/profile/childCpns/y-otherOptions/y-otherOptions.js
@@ -0,0 +1,27 @@
+// pages/profile/y-otherOptions/y-otherOptions.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        goCart() {
+            wx.switchTab({
+              url: '/pages/cart/cart',
+            })
+        }
+    }
+})
